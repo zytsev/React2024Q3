46f287be3d9355be1d45d6034ebc6a928daa458c
zytsev <zaes2000@mail.ru>
2024-07-14 21:41:47 +0100

    fix: end hooks and routing

---
+++ src/app.test.tsx
@@ -0,0 +1,10 @@
+// import App from './App';
+// import { render, screen } from '@testing-library/react';
+
+// describe('App component', () => {
+//   test('App renders', () => {
+//     render(<App />);
+
+//     expect(screen.getAllByRole()).toBeIn;
+//   });
+// });
---
+++ src/components/Btn/btn.test.tsx
@@ -0,0 +1,10 @@
+// import Btn from './Btn';
+// import { render, screen } from '@testing-library/react';
+
+// describe('Btn component', () => {
+//   test('Btn renders', () => {
+//     render(<Btn text="Search">Search</Btn>);
+
+//     expect(screen.getAllByRole('list')).toBeIn;
+//   });
+// });
